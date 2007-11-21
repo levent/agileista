96125bcd142e8fc363efdc57f145b38a441f741b
levent <levent@9000e164-a772-4a9f-9b72-ee4af0c6f47f>
2007-11-21 22:23:42 +0000

    new deploy.rb
    git-svn-id: svn+dented://www.dentedrecords.com/home/levent/repository/agileista.com/trunk@26 9000e164-a772-4a9f-9b72-ee4af0c6f47f

---
+++ config/deploy.rb
@@ -0,0 +1,48 @@
+set :application, "agileista.com"
+set :repository,  "svn+dented://dev.purebreeze.com/home/levent/repository/agileista.com/trunk"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/home/levent/apps/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+
+ssh_options[:port] = 30000
+set :sudo, "sudo -p Password:" 
+
+role :app, "agileista.purebreeze.com"
+role :web, "agileista.purebreeze.com"
+role :db,  "agileista.purebreeze.com", :primary => true
+
+namespace :deploy do
+  task :restart do
+    run "/usr/bin/mongrel_cluster_ctl stop"
+    sleep 3
+    run "/usr/bin/mongrel_cluster_ctl start"    
+    sleep 3
+    run "/usr/bin/mongrel_cluster_ctl status"
+  end
+end
+
+namespace :deploy do
+  task :stop do
+    # run "sudo /etc/init.d/apache2 stop"
+    # sleep 3
+    run "/usr/bin/mongrel_cluster_ctl stop"
+    sleep 3
+    run "/usr/bin/mongrel_cluster_ctl status"
+  end
+end
+
+namespace :deploy do
+  task :start do
+    run "/usr/bin/mongrel_cluster_ctl start"
+    sleep 3
+    run "/usr/bin/mongrel_cluster_ctl status"
+    # sleep 3
+    # run "sudo /etc/init.d/apache2 start"
+  end
+end
