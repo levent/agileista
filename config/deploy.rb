ff50bf022bdfa9bdcfc17f2837c9898fbfffeea4
levent <levent@9000e164-a772-4a9f-9b72-ee4af0c6f47f>
2008-04-10 23:30:17 +0000

    new deploy
    git-svn-id: svn+dented://www.dentedrecords.com/home/levent/repository/agileista.com/trunk@172 9000e164-a772-4a9f-9b72-ee4af0c6f47f

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, "agileista.com"
 set :repository,  "svn+dented://dev.dentedrecords.com/home/levent/repository/agileista.com/trunk"
-
+set :deploy_via, :export
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
