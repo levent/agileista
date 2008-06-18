e9e727e3ee3b7b6724f18daca189dc6590b02df8
Levent Ali <levent@leventali.com>
2008-06-18 17:11:55 +0100

    adding helper for whether user_story has tasks or not

--- app/helpers/sprint_planning_helper.rb
+++ app/helpers/sprint_planning_helper.rb
@@ -2,7 +2,14 @@ module SprintPlanningHelper
   
   def sprint_assignment(sprint)
     return "Unassigned" if sprint.blank?
-    
+  end
+  
+  def tasked?(user_story)
+    if user_story.tasks.blank?
+      return " undefined"
+    else
+      return " defined"
+    end
   end
   
 end
--- spec/helpers/sprint_planning_helper_spec.rb
+++ spec/helpers/sprint_planning_helper_spec.rb
@@ -6,6 +6,7 @@ describe SprintPlanningHelper do
   before(:each) do
     @it = self
     @sprint = Sprint.new
+    @us = UserStory.new
   end
 
   describe "#sprint_assignment" do
@@ -16,6 +17,18 @@ describe SprintPlanningHelper do
     
     it "should be nil if sprint" do
       @it.sprint_assignment(@sprint).should be_nil
+    end
+  end
+  
+  describe "#tasked?" do
+    it "should return class defined if tasked" do
+      @us.should_receive(:tasks).and_return(['stuff', 'more stuff'])
+      @it.tasked?(@us).should == " defined"
+    end
+    
+    it "should return class undefined if not tasked" do
+      @us.should_receive(:tasks).and_return([])
+      @it.tasked?(@us).should == " undefined"
     end
   end
 end
