1c7fd2b48c8f1e1b391f4c2ce2ab25fed57750ff
Levent Ali <levent@leventali.com>
2008-06-22 19:17:10 +0100

    finished get_account_name_from_request

--- app/controllers/abstract_security_controller.rb
+++ app/controllers/abstract_security_controller.rb
@@ -33,25 +33,6 @@ class AbstractSecurityController < ApplicationController
   end
   
   def get_account_name_from_request
-    'agileista'
+    params[:account_name]
   end
-  # def authenticate
-  #   case request.format
-  #   when Mime::XML, Mime::ATOM
-  #     if user = authenticate_with_http_basic { |u, p| @account.users.authenticate(u, p) }
-  #       @current_user = user
-  #     else
-  #       request_http_basic_authentication
-  #     end
-  #   else
-  #     if session_authenticated?
-  #       @current_user = @account.users.find(session[:authenticated][:user_id])
-  #     else
-  #       redirect_to(login_url) and return false
-  #     end
-  #   end
-  # end
-
-  
-  
 end
