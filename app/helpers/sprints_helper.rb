9219ff3b8c6d6e4c5d11136b736760c1ae2af7a8
Levent Ali <lebreeze@gmail.com>
2008-06-29 14:51:57 +0100

    adding sprint_header helper

--- app/helpers/sprints_helper.rb
+++ app/helpers/sprints_helper.rb
@@ -4,4 +4,9 @@ module SprintsHelper
     return "class=\"upcomingsprint\"" if sprint.upcoming?
     return ""
   end
+  
+  def sprint_header(sprint)
+    # <h1><%=h @sprint.name %> - <span class="small"><%= show_date(@sprint.start_at) %> to <%= show_date(@sprint.end_at) %> (<%= pluralize(@sprint.hours_left, 'hour') %> left)</span></h1>
+    "#{@sprint.name} - <span class=\"small\">#{show_date(@sprint.start_at)} to #{show_date(@sprint.end_at)} (#{pluralize(@sprint.hours_left, 'hour')} left)</span>"
+  end
 end
--- spec/helpers/sprints_helper_spec.rb
+++ spec/helpers/sprints_helper_spec.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe SprintsHelper do
+  include ApplicationHelper
   include SprintsHelper
   
   before(:each) do
     @it = self
     @sprint = Sprint.new
-    # @us = UserStory.new
   end
 
   describe "#identify_key_sprint" do
@@ -27,26 +27,16 @@ describe SprintsHelper do
       @it.identify_key_sprint(@sprint).should == ""
     end
   end
-  # describe "#sprint_assignment" do
-  #   it "should return 'Unassigned' if sprint is blank?" do
-  #     @sprint = nil
-  #     @it.sprint_assignment(@sprint).should == 'Unassigned'
-  #   end
-  #   
-  #   it "should be nil if sprint" do
-  #     @it.sprint_assignment(@sprint).should be_nil
-  #   end
-  # end
-  # 
-  # describe "#tasked?" do
-  #   it "should return class defined if tasked" do
-  #     @us.should_receive(:tasks).and_return(['stuff', 'more stuff'])
-  #     @it.tasked?(@us).should == " defined"
-  #   end
-  #   
-  #   it "should return class undefined if not tasked" do
-  #     @us.should_receive(:tasks).and_return([])
-  #     @it.tasked?(@us).should == " undefined"
-  #   end
-  # end
+  
+  describe "#sprint_header" do
+    it "should display the sprint name and relevant info" do
+      @sprint.start_at = 1.weeks.ago
+      @sprint.end_at = 1.weeks.from_now
+      @sprint.name = "Sprint A"
+      @sprint.stub!(:hours_left).and_return(77)
+      @it.should_receive(:show_date).with(@sprint.start_at).and_return('START')
+      @it.should_receive(:show_date).with(@sprint.end_at).and_return('END')
+      @it.sprint_header(@sprint).should == "Sprint A - <span class=\"small\">START to END (77 hours left)</span>"
+    end
+  end
 end
