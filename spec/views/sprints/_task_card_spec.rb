56b25c406d8772218ba7f67d3c48f24a0d79564c
Levent Ali <lebreeze@gmail.com>
2008-06-29 16:38:55 +0100

    adding task board view to sprints show

---
+++ spec/views/sprints/_task_card_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "sprints/_task_card" do
+  before(:each) do
+    # @task = Task.new#(:name => 'Sprint A')
+    # @task.stub!(:id).and_return(987)
+    # assigns[:task_card] = @task
+  end
+  
+  it "should be testable with local variables"
+  
+  # it "should class planned user_stories according to whether they are tasked or not" do
+  #   # template.should_receive(:tasked?).with(@us1).and_return(' defined')
+  #   # template.should_receive(:tasked?).with(@us2).and_return(' undefined')
+  #   render "sprints/_task_card"
+  #   response.should have_tag('div#com_987') do
+  #   end
+  # end
+end
