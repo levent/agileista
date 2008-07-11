de950f5cd53df3f211cd994dc564e837cfea5b07
Levent Ali <levent@leventali.com>
2008-07-11 15:53:34 +0100

    adding resolved_at date helper for impediments

--- app/helpers/impediments_helper.rb
+++ app/helpers/impediments_helper.rb
@@ -2,4 +2,12 @@ module ImpedimentsHelper
   def complete?(impediment)
     return ' class="complete"' if impediment.resolved_at
   end
+  
+  def show_resolved_at(impediment)
+    if impediment.resolved_at
+      return "#{time_ago_in_words(impediment.resolved_at)} ago"
+    else
+      link_to('Resolve', resolve_impediment_path(:id => impediment), :method => :post)
+    end
+  end
 end
