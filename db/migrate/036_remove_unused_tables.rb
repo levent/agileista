17644590f5ed3d9fc62c38fdcca9f2b5c01085b0
Levent Ali <levent@leventali.com>
2008-08-18 17:06:32 +0100

    fixing dodgy migration (trying to remove non-existent tables)

--- db/migrate/036_remove_unused_tables.rb
+++ db/migrate/036_remove_unused_tables.rb
@@ -2,10 +2,10 @@ class RemoveUnusedTables < ActiveRecord::Migration
   def self.up
     drop_table :projects
     drop_table :project_members
-    drop_table :acceptance_criterium_versions
-    drop_table :sprint_element_versions
-    drop_table :task_versions
-    drop_table :user_story_versions
+    # drop_table :acceptance_criterium_versions
+    # drop_table :sprint_element_versions
+    # drop_table :task_versions
+    # drop_table :user_story_versions
   end
 
   def self.down
