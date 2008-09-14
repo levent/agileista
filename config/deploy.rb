f81076b3d68d2316a9662208cf6c29c5b069b346
Levent Ali <lebreeze@gmail.com>
2008-09-14 09:15:08 +0100

    changing deploy.rb to most recent tag

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, "agileista.com"
 set :repository, "git@github.com:levent/agileista.git"
 set :scm, :git
 set :deploy_via, :remote_cache
-set :branch, "amsterdam_xapian"
+set :branch, "amsterdam_exceptional"
 set :scm_verbose, true
 
 # set :deploy_via, :export
