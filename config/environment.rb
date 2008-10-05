be1c80b573e05d85f08480daf98400e0966824c6
Levent Ali <lebreeze@gmail.com>
2008-10-05 18:53:42 +0100

    removing gempath from config (will reinstall using ruby enterprise)

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@
 # Uncomment below to force Rails into production mode when 
 # you don't control web/app server and can't set it the proper way
 ENV['RAILS_ENV'] ||= 'production'
-ENV['GEM_PATH']='/home/levent/.gem/ruby/1.8:/usr/lib/ruby/gems/1.8' if ENV['RAILS_ENV'] == 'production'
+# ENV['GEM_PATH']='/home/levent/.gem/ruby/1.8:/usr/lib/ruby/gems/1.8' if ENV['RAILS_ENV'] == 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.1.0'
