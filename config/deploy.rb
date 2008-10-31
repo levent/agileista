dbedc0ea674a55776fd2242afc485498bbe456b5
Levent Ali <lebreeze@gmail.com>
2008-10-31 09:13:40 +0000

    deploy.rb updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,4 @@
 set :application, "agileista.com"
-# set :repository,  "svn+dented://dev.dentedrecords.com/home/levent/repository/agileista.com/trunk"
-# git
 set :repository, "git@github.com:levent/agileista.git"
 set :scm, :git
 set :deploy_via, :remote_cache
@@ -18,10 +16,8 @@ set :deploy_to, "/home/levent/apps/#{application}"
 # set :scm, :subversion
 
 ssh_options[:port] = 30000
-# set :sudo, "sudo -p Password:" 
 set :user, "levent"
 default_run_options[:pty] = true
-# default_run_options[:pty] = true
 
 role :app, "app.agileista.com"
 role :web, "app.agileista.com"
@@ -29,26 +25,12 @@ role :db,  "app.agileista.com", :primary => true
 
 namespace :deploy do
   task :restart do
-    # run "RAILS_ENV=production #{release_path}/script/runner \"load '#{release_path}/script/ferret_stop'\""
-    # sleep 2
-    # run "/usr/bin/mongrel_cluster_ctl stop"
-    # sleep 2
-    # run "/usr/bin/mongrel_cluster_ctl start"    
-    # sleep 2
-    # run "/usr/bin/mongrel_cluster_ctl status"
-    # sleep 2
-    # run "RAILS_ENV=production #{release_path}/script/runner \"load '#{release_path}/script/ferret_start'\""
     run "touch #{release_path}/tmp/restart.txt"
   end
 end
 
 namespace :deploy do
   task :stop do
-    # run "sudo /etc/init.d/apache2 stop"
-    # sleep 3
-    # run "/usr/bin/mongrel_cluster_ctl stop"
-    # sleep 3
-    # run "/usr/bin/mongrel_cluster_ctl status"
     run "touch #{release_path}/tmp/restart.txt"
   end
 end
@@ -56,20 +38,13 @@ end
 namespace :deploy do
   task :start do
     run "touch #{release_path}/tmp/restart.txt"
-    # run "/usr/bin/mongrel_cluster_ctl start"
-    # sleep 3
-    # run "/usr/bin/mongrel_cluster_ctl status"
-    # sleep 3
-    # run "sudo /etc/init.d/apache2 start"
   end
 end
 
 task :setup_symlinks, :roles => :web do
-  # run "rm -rf #{release_path}/index"
-  # run "ln -nfs #{shared_path}/index #{release_path}/index"
   run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
-  run "rm -rf #{release_path}/vendor/plugins/acts_as_xapian/xapiandbs"
-  run "ln -nfs #{shared_path}/xapiandbs #{release_path}/vendor/plugins/acts_as_xapian/xapiandbs"
+  # run "rm -rf #{release_path}/vendor/plugins/acts_as_xapian/xapiandbs"
+  # run "ln -nfs #{shared_path}/xapiandbs #{release_path}/vendor/plugins/acts_as_xapian/xapiandbs"
 end
 
 after "deploy:update_code", :setup_symlinks
