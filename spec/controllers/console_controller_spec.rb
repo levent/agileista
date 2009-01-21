3d4536d02b5f90a91ebd243c0addaaab5245824d
Levent Ali <lebreeze@gmail.com>
2009-01-21 00:16:26 +0000

    missing files from previous commit

---
+++ spec/controllers/console_controller_spec.rb
@@ -0,0 +1,37 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ConsoleController do
+  it "should be an abstract_security_controller" do
+    controller.is_a?(AbstractSecurityController).should be_true
+  end
+  
+  it "should raise error even if you are logged in" do
+    stub_login_and_account_setup
+    lambda {get :index}.should raise_error
+  end
+  
+  it "should remain if you are levent" do
+    @person = TeamMember.new(:email => 'lebreeze@gmail.com')
+    @account = Account.new
+    @person.account = @account
+    session[:user] = 1
+    session[:account] = 1
+    Person.stub!(:find_by_id_and_account_id).and_return(@person)
+    
+    get :index
+    response.should be_success
+  end
+  
+  it "should remain if you are eben" do
+    @person = TeamMember.new(:email => 'lebreeze@gmail.com')
+    @account = Account.new
+    @person.account = @account
+    session[:user] = 1
+    session[:account] = 1
+    Person.stub!(:find_by_id_and_account_id).and_return(@person)
+    
+    get :index
+    response.should be_success
+  end
+
+end
