df56ee3955811dc254a4278e7ac083c9d5bb7c08
Levent Ali <lebreeze@gmail.com>
2009-01-23 20:10:39 +0000

    console won't crash if no account holder

---
+++ app/helpers/console_helper.rb
@@ -0,0 +1,9 @@
+module ConsoleHelper
+  def show_account_holder(account)
+    if account.account_holder
+      return "#{account.account_holder.name} #{account.account_holder.email}"
+    else
+      return "NO ACCOUNT HOLDER"
+    end
+  end
+end
