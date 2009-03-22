5bef60da8fcfcab62de86cf5e87bbe72d0dd63ab
Levent Ali <lebreeze@gmail.com>
2009-03-22 14:05:35 +0000

    removing xapian tables from db

---
+++ db/migrate/20090322140356_drop_acts_as_xapian.rb
@@ -0,0 +1,14 @@
+class DropActsAsXapian < ActiveRecord::Migration
+  def self.up
+    drop_table :acts_as_xapian_jobs
+  end
+
+  def self.down
+    create_table :acts_as_xapian_jobs do |t|
+      t.column :model, :string, :null => false
+      t.column :model_id, :integer, :null => false
+      t.column :action, :string, :null => false
+    end
+    add_index :acts_as_xapian_jobs, [:model, :model_id], :unique => true
+  end
+end
