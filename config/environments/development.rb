3137810a2ab981009a51d10c95c3f98715c1ceb0
Levent Ali <lebreeze@gmail.com>
2009-03-23 03:52:37 +0000

    giving dev env same cookie options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,11 @@ config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
 # config.action_view.cache_template_extensions         = false
 config.action_view.debug_rjs                         = true
+config.action_controller.session = {
+  :key => '_fcloud_dev_session',
+  :secret      => '3db594b389adf2e6b07ca41349e1dd55984f0ac7a3de3647220d6d44b97b720614714aaf24ef81aaf07b60418d262af93e26b73733102d6bdd126ab45c299baa',
+  :expire_after => 24.hours
+}
 ActionController::Base.session_options[:session_domain] = 'agileista.local'
 
 # Don't care if the mailer can't send
