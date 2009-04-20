86bca1314b4a980491d2c6c2e23c233588f90856
Levent Ali (nyc) <lebreeze@gmail.com>
2009-04-20 23:05:38 +0100

    zIndex on draggables plus background of #fff on all cards

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,7 +19,7 @@ function set_flash(message) {
 function setupTaskBoard(user_story_id) {
   var us_container = '#user_story_container_' + user_story_id;
   $(us_container).find('div.notecard').each(function(card) {
-    $(this).draggable({delay:400});
+    $(this).draggable({delay: 100, zIndex: 100});
   });
 
   $("#incomplete_"+user_story_id).droppable({
@@ -56,7 +56,7 @@ function setupTaskBoard(user_story_id) {
 
 function setupSprintPlanningDefaults(sprint_id) {
   $('.notecard').each(function(card) {
-    $(this).draggable({delay: 200, revert: false});
+    $(this).draggable({delay: 100, zIndex: 100, revert: false});
   });
   
   $('#estimated').droppable({
