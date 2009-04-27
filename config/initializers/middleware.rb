714486b4b3f1bcd1335fedc4e3fe5d5472e9eeec
Gerhard Lazu <mail@gerhardlazu.com>
2009-04-27 11:29:16 +0100

    Added Rack::Bug http://www.brynary.com/2009/4/22/rack-bug-debugging-toolbar-in-four-minutes

---
+++ config/initializers/middleware.rb
@@ -0,0 +1,7 @@
+# config/initializers/middleware.rb
+require "rack/bug"
+
+ActionController::Dispatcher.middleware.use Rack::Bug,
+  :ip_masks   => [IPAddr.new("127.0.0.1")],
+  :secret_key => "epT5uCIchlsHCeR9dloOeAPG66PtHd9K8l0q9avitiaA/KUrY7DE52hD4yWY+8z1",
+  :password   => "buggeroff"
