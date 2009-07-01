948d1df6bfbc87df69a2252e2efac2f9072e2321
Levent <lebreeze@gmail.com>
2009-07-01 09:59:29 +0100

    sprint end date is end of day

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -63,7 +63,7 @@ class Sprint < ActiveRecord::Base
   
   def calculate_end_date
     unless self.end_at
-      self.end_at = self.account.iteration_length.to_i.weeks.from_now(1.day.ago(self.start_at))
+      self.end_at = self.account.iteration_length.to_i.weeks.from_now(1.day.ago(self.start_at)).end_of_day
     end
   end
   
