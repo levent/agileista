643f96543d5a0086e5f05742f9ec581c7afdd740
Levent <lebreeze@gmail.com>
2009-09-03 11:03:26 +0100

    more smtp experiments

--- config/environment.rb
+++ config/environment.rb
@@ -70,12 +70,12 @@ end
 # end
 
 # Include your application configuration below
-ActionMailer::Base.delivery_method = :sendmail
-
-ActionMailer::Base.sendmail_settings = {
-:location       => '/usr/sbin/sendmail',
-:arguments      => '-i -t'
-}
+# ActionMailer::Base.delivery_method = :sendmail
+# 
+# ActionMailer::Base.sendmail_settings = {
+# :location       => '/usr/sbin/sendmail',
+# :arguments      => '-i -t'
+# }
 #ActionMailer::Base.perform_deliveries = true
 #ActionMailer::Base.raise_delivery_errors = true
 #ActionMailer::Base.server_settings = {
