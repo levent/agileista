780b33ec282a2d73712d914303930b2e450b9453
Levent Ali <lebreeze@gmail.com>
2009-12-27 09:06:54 +0000

    make signup the home page

--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,9 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.root :controller => "beta_emails"
-  map.beta 'beta', :controller => 'beta_emails'
+  # map.root :controller => "beta_emails"
+  map.root :controller => "signup"
+  # map.beta 'beta', :controller => 'beta_emails'
   # map.connect 'project/overview/:id', :controller => "project/overview", :action => 'index'
 
   # Allow downloading Web Service WSDL as a file with an extension
