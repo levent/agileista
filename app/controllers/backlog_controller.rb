ae8d82a0e1c0c27ddce7b7fcceff9c6c66e127d0
Levent Ali <lebreeze@gmail.com>
2010-03-11 17:19:11 +0000

    bug fixes around csv export

--- app/controllers/backlog_controller.rb
+++ app/controllers/backlog_controller.rb
@@ -42,24 +42,4 @@ class BacklogController < AbstractSecurityController
     end
     render :json => {:ok => true}.to_json
   end
-  
-  private
-  
-  def render_csv(filename = nil)
-    filename ||= params[:action]
-    filename += '.csv'
-
-    if request.env['HTTP_USER_AGENT'] =~ /msie/i
-      headers['Pragma'] = 'public'
-      headers["Content-type"] = "text/plain" 
-      headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
-      headers['Content-Disposition'] = "attachment; filename=\"#{filename}\"" 
-      headers['Expires'] = "0" 
-    else
-      headers["Content-Type"] ||= 'text/csv'
-      headers["Content-Disposition"] = "attachment; filename=\"#{filename}\"" 
-    end
-
-    render :layout => false
-  end
 end
