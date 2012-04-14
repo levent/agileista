941fb623e60484095d16803c5702c084d2607b34
Levent Ali <lebreeze@gmail.com>
2012-04-14 16:16:05 +0100

    Old school ruby hash syntax

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 Agileista::Application.config.session_store :cookie_store,
-  key: '_agileista_session',
-  domain: COOKIE_DOMAIN
+  :key => '_agileista_session',
+  :domain => COOKIE_DOMAIN
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
