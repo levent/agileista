61064f3693cec1db89ddde6fff5cf2f5ff6d19bb
Levent Ali <lebreeze@gmail.com>
2012-04-16 15:54:40 +0100

    Simplify restart

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -35,9 +35,7 @@ role :db,  "fukushima", :primary => true
 namespace :deploy do
   task :restart do
     run "cd #{current_path} && bundle exec bundle exec rake assets:precompile RAILS_ENV=#{rails_env}"
-    run "/etc/init.d/agileista restart 3000"
-    run "/etc/init.d/agileista restart 3001"
-    run "/etc/init.d/agileista restart 3002"
+    run "/etc/init.d/agileista restart"
   end
 
   # task :bundle do
