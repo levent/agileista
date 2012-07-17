3d932044fb70813149c1c8dee9a21fde43e6f753
Levent Ali <lebreeze@gmail.com>
2012-07-17 07:44:16 +0100

    removing legacy code

--- config copy/initializers/middleware.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/middleware.rb
-require "rack/bug"
-
-ActionController::Dispatcher.middleware.use Rack::Bug,
-  :ip_masks   => [IPAddr.new("127.0.0.1")],
-  :secret_key => "eR9dloOeAPepT5uCIchlsHCG66PtHd9K8l0q9avitiaA/KUrY72hDDE54yWY+8z1",
-  :password   => "istanbul"
