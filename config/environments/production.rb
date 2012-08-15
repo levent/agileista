0a4810858c607674a815cd065b20508336ee2c63
Levent Ali <lebreeze@gmail.com>
2012-08-15 17:54:39 +0100

    ssl cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Agileista::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "https://c3458723.ssl.cf0.rackcdn.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
