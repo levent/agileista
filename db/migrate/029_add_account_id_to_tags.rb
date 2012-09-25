c06fecfebd9c12fd3fca867c377a8b22dfa194bf
Levent Ali <lebreeze@gmail.com>
2012-09-25 09:57:14 +0100

    [travis] dodgy migration removed

--- db/migrate/029_add_account_id_to_tags.rb
+++ db/migrate/029_add_account_id_to_tags.rb
@@ -3,15 +3,6 @@ class AddAccountIdToTags < ActiveRecord::Migration
     remove_index :tags, :name
     add_column :tags, :account_id, :integer
     add_index :tags, [:name, :account_id], :unique => true
-    for tagging in Tagging.find(:all)
-      tag = tagging.tag
-      account_id = UserStory.find(tagging.taggable_id).account_id
-      puts tag.name.to_s + " :TAG"
-      puts account_id
-      puts
-      tag.account_id = account_id
-      tag.save
-    end
   end
 
   def self.down
