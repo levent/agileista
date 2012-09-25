cabdb8ccfa45c71828dfba41c000861165723551
Levent Ali <lebreeze@gmail.com>
2012-09-25 10:28:20 +0100

    Rename conflicting velocity

--- db/migrate/20090420220929_create_velocity.rb
+++ db/migrate/20090420220929_create_velocity.rb
@@ -1,4 +1,4 @@
-class Velocity < ActiveRecord::Migration
+class CreateVelocity < ActiveRecord::Migration
   def self.up
     add_column :sprints, :velocity, :integer, :default => nil
     add_column :accounts, :velocity, :integer, :default => nil
