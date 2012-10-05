b87f8051b85b4d3a8046751fac0f0047f4a9de4c
Levent Ali <lebreeze@gmail.com>
2012-10-05 14:21:44 +0100

    Fix theme specs

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,6 +1,5 @@
 class Theme < ActiveRecord::Base
 
-  # acts_as_list :scope => :account
   include RankedModel
   ranks :position,
     :with_same => :account_id
@@ -11,20 +10,14 @@ class Theme < ActiveRecord::Base
   has_many :themings, :foreign_key => "theme_id"
   has_many :user_stories, :through => :themings
 
-  # has_many_polymorphs :themables,
-  #     :from => [:user_stories],
-  #     :through => :themings,
-  #     :dependent => :destroy
-      
   def story_points
     @story_points = 0
     self.user_stories.collect{|x| @story_points += x.story_points if x.story_points}
     return @story_points
   end
-  
+
   def story_points_left
     @story_points = self.story_points
-    # self.user_stories.find(:all, :conditions => ["done = ? AND sprint_id IS ?", 0, nil]).collect{|x| @story_points += x.story_points if x.story_points}
     self.user_stories.each do |us|
       @story_points -= us.story_points if us.story_points && us.complete?
     end
--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -3,26 +3,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Theme do
   it {should belong_to(:account)}
   it {should validate_presence_of(:name)}
-  
-  describe "acts as list" do
-    before(:each) do
-      Theme.destroy_all
-    end
-    
-    it "should increment on create" do
-      Theme.create!(:name => "T1").position.should == 1
-      Theme.create!(:name => "T2").position.should == 2
-    end
-  end
-  
+
   context "story points left" do
-    before(:each) do
-      @theme = Theme.make
-      @us1 = UserStory.make(:story_points => 13, :themes => [@theme])
-      @us2 = UserStory.make(:story_points => nil, :themes => [@theme], :tasks => [Task.make])
-      @us3 = UserStory.make(:story_points => 8, :themes => [@theme], :tasks => [Task.make])
+    before do
+      @account = Account.make!
+      @theme = Theme.make!
+      @us1 = UserStory.make!(:story_points => 13, :themes => [@theme], :account => @account)
+      @us2 = UserStory.make!(:story_points => nil, :themes => [@theme], :tasks => [Task.make], :account => @account)
+      @us3 = UserStory.make!(:story_points => 8, :themes => [@theme], :tasks => [Task.make], :account => @account)
     end
-    
+
     it "should tell you how many story points are left to complete" do
       complete_task = @us3.tasks.first
       complete_task.hours = 0
@@ -30,7 +20,7 @@ describe Theme do
       @us3.complete?.should be_true
       @theme.story_points_left.should == 13
     end
-    
+
     it "should treat null story points as 0" do
       complete_task = @us2.tasks.first
       complete_task.hours = 0
