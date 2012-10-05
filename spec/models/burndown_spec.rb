ee585c63e753cae25042410380cb427dd54bc4ee
Levent Ali <lebreeze@gmail.com>
2012-10-05 14:38:41 +0100

    Fix burndown spec

--- spec/models/burndown_spec.rb
+++ spec/models/burndown_spec.rb
@@ -6,11 +6,13 @@ describe Burndown do
 
   context "sort order" do
     it "should be ordered by date by default" do
-      b1 = Burndown.make(:created_on => 3.days.ago)
-      b4 = Burndown.make(:created_on => 6.days.ago)
-      b2 = Burndown.make(:created_on => 4.days.ago)
-      b3 = Burndown.make(:created_on => 5.days.ago)
-      b5 = Burndown.make(:created_on => 7.days.ago)
+      account = Account.make!
+      sprint = Sprint.make!(:account => account)
+      b1 = Burndown.make!(:created_on => 3.days.ago, :sprint => sprint)
+      b4 = Burndown.make!(:created_on => 6.days.ago, :sprint => sprint)
+      b2 = Burndown.make!(:created_on => 4.days.ago, :sprint => sprint)
+      b3 = Burndown.make!(:created_on => 5.days.ago, :sprint => sprint)
+      b5 = Burndown.make!(:created_on => 7.days.ago, :sprint => sprint)
       Burndown.all.should == [b5, b4, b3, b2, b1]
     end
   end
