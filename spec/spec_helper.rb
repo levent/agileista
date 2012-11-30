1dc8c3a8fe38c2daec007990b06ee2765eec2951
Levent Ali <lebreeze@gmail.com>
2012-11-30 15:33:28 +0000

    min cov 56

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'simplecov'
 SimpleCov.start 'rails'
-SimpleCov.minimum_coverage 60
+SimpleCov.minimum_coverage 56
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
