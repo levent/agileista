bf0be55f75e7c1fc9e36c33c1ae5f465d0b5ae1c
Levent Ali <lebreeze@gmail.com>
2013-02-01 16:59:36 +0000

    Simplifying views

--- app/helpers/sprints_helper.rb
+++ app/helpers/sprints_helper.rb
@@ -10,16 +10,9 @@ module SprintsHelper
   end
 
   def sprint_header(sprint, options = {})
-    options = {:buttons => []}.merge(options)
-
-    if options[:buttons].any? && !sprint.finished?
-      buttons = sprint_buttons(sprint, options[:buttons])
-      result = [%{<span class="tab">#{sprint.name} #{buttons}</span>}]
-    else
-      result = [%{<span class="tab">#{sprint.name}</span>}]
-    end
-    result << %{<span class="hightlight">#{show_date(sprint.start_at)} to #{show_date(sprint.end_at)}</span>}
-    result << %{<span class="hightlight"><strong id="current_complete"></strong> out of <strong id="current_total">#{sprint.total_story_points}</strong> story points completed (<strong id="current_percentage"></strong>)</span>} unless sprint.user_stories.blank?
+    result = [%{#{sprint.name}}]
+    result << %{<small>#{show_date(sprint.start_at)} to #{show_date(sprint.end_at)}</small>}
+    result << %{<span id="current_complete"></span> <small>out of</small> <span id="current_total">#{sprint.total_story_points}</span> <small>story points completed (<span id="current_percentage"></span>)</small>} unless sprint.user_stories.blank?
 
     return result.join(" ").html_safe
   end
