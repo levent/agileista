6503ff0d770d5df7f377e8caa43746f35240a8ee
Levent Ali <lebreeze@gmail.com>
2013-02-08 19:57:22 +0000

    Removing cruft

--- app/helpers/sprints_helper.rb
+++ app/helpers/sprints_helper.rb
@@ -16,18 +16,6 @@ module SprintsHelper
     return result.join(" ").html_safe
   end
 
-  def sprint_buttons(sprint, buttons = [])
-    result = []
-
-    if buttons.any?
-      result << link_to("Edit", edit_sprint_url(sprint), :class => "button") if buttons.include?(:edit)
-      result << link_to("Plan", edit_sprint_url(sprint), :class => "button") if buttons.include?(:plan)
-      result << link_to("Add story", new_sprint_user_story_url(sprint), :class => "button") if buttons.include?(:add_story)
-    end
-
-    return result.join(" ") if result.any?
-  end
-
   def average_velocity(account, opts = {})
     return nil unless velocity = @velocity || account.average_velocity
     options = {:unit => "story points"}.merge(opts)
