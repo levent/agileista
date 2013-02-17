5eb24949fdaef5e63b4f3ec7a66a986b2220e59e
Levent Ali <lebreeze@gmail.com>
2013-02-17 13:56:56 +0000

    [assets] get s3 sync to work

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -3,10 +3,10 @@ if defined?(AssetSync)
     config.fog_provider = 'AWS'
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
     config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-    config.fog_directory = ENV['FOG_DIRECTORY']
+    config.fog_directory = 'agileista-production'
 
     # Increase upload performance by configuring your region
-    # config.fog_region = 'eu-west-1'
+    config.fog_region = 'eu-west-1'
     #
     # Don't delete files from the store
     config.existing_remote_files = "delete"
