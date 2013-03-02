9e9fb97b111507eaf87587913737219261b5c79f
Levent Ali <lebreeze@gmail.com>
2013-03-02 12:12:07 +0000

    require 'spec_helper'

--- spec/integration/controllers/user_stories_controller_spec.rb
+++ spec/integration/controllers/user_stories_controller_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe UserStoriesController do
   it "should be an abstract_security_controller" do
--- spec/integration/models/release_spec.rb
+++ spec/integration/models/release_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe UserStory do
   it {should validate_presence_of(:account_id)}
