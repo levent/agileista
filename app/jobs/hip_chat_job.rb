2857094c20fbd938e00086549a0f6719624a4389
Levent Ali <lebreeze@gmail.com>
2013-04-08 10:42:53 +0100

    [hipchat] notify setting

--- app/jobs/hip_chat_job.rb
+++ app/jobs/hip_chat_job.rb
@@ -1,8 +1,8 @@
 class HipChatJob
   @queue = :hip_chat
 
-  def self.perform(api_token, room, message)
+  def self.perform(api_token, room, notify, message)
     client = HipChat::Client.new(api_token)
-    client[room].send('Agileista', message, :color => 'purple')
+    client[room].send('Agileista', message, :color => 'purple', :notify => notify)
   end
 end
