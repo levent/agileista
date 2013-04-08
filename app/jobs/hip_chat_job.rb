7e31bdac5deeec9aa339c266efcc3804bfa5c97a
Levent Ali <lebreeze@gmail.com>
2013-04-08 12:11:49 +0100

    [hipchat] job robuster

--- app/jobs/hip_chat_job.rb
+++ app/jobs/hip_chat_job.rb
@@ -1,7 +1,7 @@
 class HipChatJob
   @queue = :hip_chat
 
-  def self.perform(api_token, room, notify, message)
+  def self.perform(api_token, room, notify = false, message = '')
     client = HipChat::Client.new(api_token)
     client[room].send('Agileista', message, :color => 'purple', :notify => notify)
   end
