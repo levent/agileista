45be74abb7d79f98745f5f4ab8f35eb7daa1569b
Levent Ali <lebreeze@gmail.com>
2013-04-15 20:27:09 +0100

    Try and get this resque shit to work

--- app/jobs/calculate_burn_job.rb
+++ app/jobs/calculate_burn_job.rb
@@ -2,8 +2,7 @@ class CalculateBurnJob
   @queue = :calculate_burn
 
   def self.perform(date, sprint_id)
-    burndown = Burndown.where(:sprint_id => sprint_id, :created_on => date).first
-    burndown ||= Burndown.new(:sprint_id => sprint_id, :created_on => date)
+    burndown = Burndown.find_or_create_by_sprint_id_and_created_on(sprint_id, date)
     burndown.hours_left = burndown.sprint.hours_left
     burndown.story_points_complete = burndown.sprint.story_points_burned
     burndown.story_points_remaining = burndown.sprint.total_story_points
--- lib/tasks/resque.rake
+++ lib/tasks/resque.rake
@@ -4,6 +4,8 @@ require 'resque/tasks'
 namespace :resque do
   puts "Loading Rails environment for Resque"
   task :setup => :environment do
+    require 'sprint'
+    require 'burndown'
     ActiveRecord::Base.descendants.each { |klass|  klass.columns }
   end
 end
