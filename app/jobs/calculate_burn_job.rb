e5ebaf69e37dffc2263a9d4d90cfcdef8076ec56
Levent Ali <lebreeze@gmail.com>
2013-04-15 18:00:08 +0100

    Fix burn job bug

--- app/jobs/calculate_burn_job.rb
+++ app/jobs/calculate_burn_job.rb
@@ -1,7 +1,7 @@
 class CalculateBurnJob
   @queue = :calculate_burn
 
-  def self.perform(sprint, date)
+  def self.perform(date, sprint)
     burndown = Burndown.find_or_create_by_sprint_id_and_created_on(sprint.id, date)
     burndown.hours_left = sprint.hours_left
     burndown.story_points_complete = sprint.story_points_burned
