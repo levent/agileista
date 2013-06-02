8548ef96c1d7fd377e558e3da9cced85c0e0e78f
Levent Ali <lebreeze@gmail.com>
2013-06-02 11:20:41 +0100

    Try page caching

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,13 @@
 class AboutController < ApplicationController
+  before_filter :redirect_to_projects
+  caches_page :index
+
   def index
+  end
+
+  private
+
+  def redirect_to_projects
     redirect_to projects_path if person_signed_in? && request.path != "/about"
   end
 end
