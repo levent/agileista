bdf2f899ec346aa53d47d72f88a610f7a9dfb068
Levent Ali <lebreeze@gmail.com>
2014-02-18 16:40:16 +0000

    Minor deprecation

--- app/workers/calculate_burn_worker.rb
+++ app/workers/calculate_burn_worker.rb
@@ -2,7 +2,7 @@ class CalculateBurnWorker
   include Sidekiq::Worker
 
   def perform(date, sprint_id)
-    burndown = Burndown.find_or_create_by_sprint_id_and_created_on(sprint_id, date)
+    burndown = Burndown.find_or_create_by(sprint_id: sprint_id, created_on: date)
     burndown.hours_left = burndown.sprint.hours_left
     burndown.story_points_complete = burndown.sprint.story_points_burned
     burndown.story_points_remaining = burndown.sprint.total_story_points
