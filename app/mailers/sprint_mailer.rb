0b2c8f3fbbeaa364217df8b42e7f61ee7ef60a44
Levent Ali <lebreeze@gmail.com>
2014-02-28 09:34:57 +0000

    Sprint Mailer, better body

--- app/mailers/sprint_mailer.rb
+++ app/mailers/sprint_mailer.rb
@@ -1,9 +1,11 @@
 class SprintMailer < ActionMailer::Base
   default from: EMAIL_FROM
 
-  def summary_email(person, sprint)
+  def summary_email(person, sprint, project)
     @person = person
     @sprint = sprint
+    @project = project
+    @sprint_url = project_sprint_path(project, sprint)
     @complete = sprint.user_stories.select {|us| us.status == "complete" }
     @inprogress = sprint.user_stories.select {|us| us.status == "inprogress" }
     @incomplete = sprint.user_stories.select {|us| us.status == "incomplete" }
