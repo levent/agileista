1294e668fe724a5732b80889399858206c12ecfe
Levent Ali <lebreeze@gmail.com>
2014-02-28 09:58:05 +0000

    schedule

--- app/workers/summary_email_worker.rb
+++ app/workers/summary_email_worker.rb
@@ -2,7 +2,11 @@ class SummaryEmailWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { daily }
+  recurrence do
+    weekly.day(:monday, :tuesday, :wednesday, :thursday, :friday).hour_of_day(18)
+
+    weekly.day(:monday, :tuesday, :wednesday, :thursday, :friday).hour_of_day(10)
+  end
 
   def perform
     person = Person.find_by(email: 'lebreeze@gmail.com')
