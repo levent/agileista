c8244267a8805889a6c4f534ef932a23a324deef
Levent Ali <lebreeze@gmail.com>
2014-03-02 08:58:25 +0000

    Move logic

--- app/mailers/sprint_mailer.rb
+++ app/mailers/sprint_mailer.rb
@@ -5,6 +5,7 @@ class SprintMailer < ActionMailer::Base
     @person = person
     @sprint = sprint
     @project = sprint.project
+    person.update_attribute(:unsubscribe_token, SecureRandom.urlsafe_base64(25)) if person.unsubscribe_token.nil?
     @unsubscribe_token = person.unsubscribe_token
     @sprint_url = project_sprint_url(@project, sprint)
     @complete = sprint.user_stories.select {|us| us.status == "complete" }
