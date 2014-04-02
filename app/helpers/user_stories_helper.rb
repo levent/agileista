89fd3ae7054584f2d4df07e70affe167937f522f
Levent Ali <lebreeze@gmail.com>
2014-04-02 17:32:31 +0100

    [ui] approved - highlight green

--- app/helpers/user_stories_helper.rb
+++ app/helpers/user_stories_helper.rb
@@ -5,7 +5,8 @@ module UserStoriesHelper
     definition.scan(/\[\w+\]/).uniq.each do |m|
       tag = m.clone
       tag.gsub!('[', '').gsub!(']', '')
-      link = link_to m, project_search_path(@project, :q => "tag:#{tag.downcase}"), :class => 'tagged'
+      m == '[APPROVED]' ? link_text = "[<span class=\"highlight-green\">#{tag}</span>]".html_safe : link_text = m
+      link = link_to link_text, project_search_path(@project, :q => "tag:#{tag.downcase}"), :class => 'tagged'
       definition.gsub!(m, link).html_safe
     end
     definition
