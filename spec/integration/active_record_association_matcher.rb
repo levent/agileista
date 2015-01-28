a8b0e10ffd3787ae7e12cd892952255d26218c97
Levent Ali <lebreeze@gmail.com>
2015-01-28 14:59:29 +0000

    [minor] Remove whitespace

--- spec/integration/active_record_association_matcher.rb
+++ spec/integration/active_record_association_matcher.rb
@@ -19,12 +19,12 @@ module ActiveRecordAssociationMatcher
         else
           return false
         end
-            
+
       rescue
         return false
       end
     end
-    
+
     def with(options)
       @expected_options.merge!(options)
       return self
@@ -42,59 +42,59 @@ module ActiveRecordAssociationMatcher
       return message
     end
   end
-  
+
   class BelongTo < AbstractAssociationMatcher
     def expected_macro
       :belongs_to
     end
-    
+
     def expected_macro_description
       "belong to"
     end
   end
-  
+
   class HaveMany < AbstractAssociationMatcher
     def expected_macro
       :has_many
     end
-    
+
     def expected_macro_description
       "have many"
     end
   end
-  
+
   class HaveAndBelongToMany < AbstractAssociationMatcher
     def expected_macro
       :has_and_belongs_to_many
     end
-    
+
     def expected_macro_description
       "have and belong to many"
     end
   end
-  
+
   class HaveOne < AbstractAssociationMatcher
     def expected_macro
       :has_one
     end
-    
+
     def expected_macro_description
       "have one"
     end
   end
 
   def belong_to(expected)
-    BelongTo.new(expected) 
+    BelongTo.new(expected)
   end
-  
+
   def have_many(expected)
     HaveMany.new(expected)
   end
-  
+
   def have_and_belong_to_many(expected)
     HaveAndBelongToMany.new(expected)
   end
-  
+
   def have_one(expected)
     HaveOne.new(expected)
   end
