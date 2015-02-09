3fb65e4737a7edc535fdbd2f2f61b12b213a205d
Levent Ali <lebreeze@gmail.com>
2015-02-09 14:34:41 +0000

    [rubocop] tidy up person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,28 +1,24 @@
 require 'digest/sha1'
 
 class Person < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :confirmable
 
-  # Setup accessible (or protected) attributes for your model
-# attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   include Gravtastic
   gravtastic
 
-  validates_presence_of :name
+  validates :name, presence: true
   has_many :user_stories
   has_many :task_developers, foreign_key: "developer_id"
   has_many :tasks, through: :task_developers
   has_many :team_members, dependent: :destroy
-  has_many :projects, -> {order('LOWER(projects.name)')}, through: :team_members
+  has_many :projects, -> { order('LOWER(projects.name)') }, through: :team_members
 
   # Used to migrate pre-devise users
   def valid_password?(password)
-    if self.hashed_password.present?
-      if self.hashed_password == self.encrypt(password)
+    if hashed_password.present?
+      if hashed_password == encrypt(password)
         self.password = password
         self.hashed_password = nil
         self.confirm!
@@ -37,7 +33,7 @@ class Person < ActiveRecord::Base
   end
 
   def encrypt(password)
-    Digest::SHA1.hexdigest("#{self.salt}--#{password}")
+    Digest::SHA1.hexdigest("#{salt}--#{password}")
   end
 
   def scrum_master_for?(project)
@@ -47,7 +43,7 @@ class Person < ActiveRecord::Base
       REDIS.set("project:#{project.id}:scrum_master", scrum_master_email)
       REDIS.expire("project:#{project.id}:scrum_master", REDIS_EXPIRY)
     end
-    scrum_master_email == self.email
+    scrum_master_email == email
   end
 
   def admin?
