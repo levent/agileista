e16b11dc9a11ffd8bc7259d039cd48134b448950
Levent Ali <lebreeze@gmail.com>
2015-02-10 13:30:13 +0000

    Add some test coverage for invitations

--- spec/features/create_invitation_spec.rb
+++ spec/features/create_invitation_spec.rb
@@ -30,4 +30,30 @@ RSpec.feature 'Create invitations', type: :feature do
     click_button 'Send invite'
     expect(page).to have_content 'Invalid email address'
   end
+
+  context 'with existing user' do
+    before do
+      @other_user = create_person
+      create_project(@other_user)
+    end
+
+    it 'adds them to new project' do
+      visit "/projects/#{@project.id}/invitations/new"
+      fill_in 'Email', :with => @other_user.email
+      click_button 'Send invite'
+      expect(page).to have_content "#{@other_user.email}"
+    end
+  end
+
+  context 'with existing invite' do
+    before do
+      Invitation.create!(email: 'newguy@example.com', project: @project)
+    end
+
+    it 'should allow deleting the invite' do
+      visit "/projects/#{@project.id}/people"
+      click_link "Remove invitation"
+      expect(Invitation.where(email: 'newguy@example.com')).to be_empty
+    end
+  end
 end
