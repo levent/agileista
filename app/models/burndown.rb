b2967745aac4f5b67b4226b1ff4a6a21b033b7fb
Levent Ali <lebreeze@gmail.com>
2015-02-10 10:18:08 +0000

    [refactor] Burndown.generate! added

--- app/models/burndown.rb
+++ app/models/burndown.rb
@@ -3,4 +3,12 @@ class Burndown < ActiveRecord::Base
   validates :sprint_id, presence: true
 
   default_scope { order("created_on") }
+
+  def self.generate!(sprint, date)
+    burndown = Burndown.find_or_create_by(sprint_id: sprint.id, created_on: date)
+    burndown.hours_left = sprint.hours_left
+    burndown.story_points_complete = sprint.story_points_burned
+    burndown.story_points_remaining = sprint.total_story_points
+    burndown.save!
+  end
 end
--- app/workers/calculate_burn_worker.rb
+++ app/workers/calculate_burn_worker.rb
@@ -2,10 +2,6 @@ class CalculateBurnWorker
   include Sidekiq::Worker
 
   def perform(date, sprint_id)
-    burndown = Burndown.find_or_create_by(sprint_id: sprint_id, created_on: date)
-    burndown.hours_left = burndown.sprint.hours_left
-    burndown.story_points_complete = burndown.sprint.story_points_burned
-    burndown.story_points_remaining = burndown.sprint.total_story_points
-    burndown.save!
+    Burndown.generate!(Sprint.find(sprint_id), date)
   end
 end
