8fac0999f43b95cbeb8b352819aa3f88b3f254b3
Levent Ali <lebreeze@gmail.com>
2017-06-06 16:21:31 +0100

    Correct jquery-ui require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui/sortable
-//= require jquery-ui/draggable
-//= require jquery-ui/droppable
+//= require jquery-ui/widgets/sortable
+//= require jquery-ui/widgets/draggable
+//= require jquery-ui/widgets/droppable
 //= require foundation/foundation
 //= require foundation/foundation.topbar
 //= require foundation/foundation.reveal
